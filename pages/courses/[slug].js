be0e75682a99535d91e1f341199ac2cd8d5e036c
Zoheb Ahmed <zohebcool1542@gmail.com>
2022-09-12 20:20:20 +0530

    frontend done

---
+++ pages/courses/[slug].js
@@ -0,0 +1,45 @@
+import { Modal } from '@components/ui/common'
+import { CourseHero, Curriculum, Keypoints } from '@components/ui/course'
+import { BaseLayout } from '@components/ui/layout'
+import { getAllCourse } from '@content/courses/fetcher'
+
+export default function Course({ course }) {
+  return (
+    <>
+      <div className='py-4'>
+        <CourseHero
+          title={course.title}
+          description={course.description}
+          image={course.coverImage}
+        />
+      </div>
+      <Keypoints points={course.wsl} />
+      <Curriculum locked={true} />
+      <Modal />
+    </>
+  )
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourse()
+  return {
+    paths: data.map((c) => ({
+      params: {
+        slug: c.slug,
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourse()
+  const course = data.filter((c) => c.slug === params.slug)[0]
+  return {
+    props: {
+      course,
+    },
+  }
+}
+
+Course.Layout = BaseLayout
